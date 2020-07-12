d80876d04a11b75f5653fbbabed806f45ce0d452
Thrashm <thrashm@github.com>
2020-07-12 12:34:05 -0700

    Pass items to vec, dump vec when ready

--- threading/src/main.rs
+++ threading/src/main.rs
@@ -7,29 +7,49 @@ use std::{
     sync::mpsc::channel,
 };
 
+struct Thing {
+    value: String,
+}
+
+impl Drop for Thing {
+    fn drop(&mut self) { 
+        println!("{}", self.value);
+     }
+}
+
+
+const cleanUpCount: u8 = 15;
+
 fn main() {
     let (sender, receiver) = channel();
 
     spawn(move || {
-        let val = String::from("!!!");
-        sleep(Duration::from_secs(2));
-        sender.send(Some(String::from("hi"))).unwrap();
-        sleep(Duration::from_secs(2));
-        sender.send(Some(String::from("nanners"))).unwrap();
-        sleep(Duration::from_secs(2));
-        sender.send(Some(val)).unwrap();
+        for n in 1..=20 {
+            sleep(Duration::from_millis(250));
+            println!("Pushing item");
+            sender.send(Some(Thing { value: String::from(format!("Item {}", n)) })).unwrap();
+        }
         sender.send(None).unwrap();
     });
 
+    let mut count = 0;
+    let mut dump: Vec<Thing> = Vec::new();
     'loopin: loop {
         let receiver_result = receiver.recv();
     
         match receiver_result {
             Ok(received) => match received {
-                Some(value) => println!("Get: {}", value),
+                Some(thing) => dump.push(thing),
                 None => break 'loopin,
             },
             Err(err) => eprint!("Error: {}", err),
+        }
+        count = count + 1;
+        if count >= cleanUpCount {
+            println!("Cleaning up resources");
+            count = 0;
+            dump = Vec::new();
+            println!("Resources cleaned up");
         }
     }
 }
